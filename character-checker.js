a73895357fa343e52032b56b77951fbabc60e961
JesusC-LaunchCode <139182839+JesusC-LaunchCode@users.noreply.github.com>
2026-07-28 15:22:58 -0400

    Update character-checker.js

--- character-checker.js
+++ character-checker.js
@@ -1,9 +1,9 @@
 const input = require('readline-sync');
 
 let characterString = "Web Development is fun";
-let indexNumber = input.question("Please choose a number to find the character at that index: ");
+let indexNumber = input.question("Please choose a number to find the character at that index: "); //Asking user to select a character to locate the index at that number
 
 
-let indexCharacter = characterString[indexNumber];
-console.log("You chose number: " + indexNumber + ". " + "The index character in that index number is: " + indexCharacter);
+let indexCharacter = characterString[indexNumber]; //Getting the number from the user(indexNumber) and searching the string(CharacterString) to get the character.
+console.log("You chose number: " + indexNumber + ". " + "The index character in that index number is: " + indexCharacter); //Printing out the users choice and the character at that index
 
